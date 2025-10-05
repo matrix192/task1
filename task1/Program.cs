935e2dc841902b920abecf7a6f0eb70f9640a073
oguzok14 <kviantos1@mail.ru>
2025-10-05 15:44:21 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,55 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+
+            double ans = 0;
+            Console.Write("Введите первое число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите арифметическое действие: ");
+            string operation = Console.ReadLine()!;
+            Console.WriteLine("Введите второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "+":
+                    ans = a + b;
+                    break;
+                case "-":
+                    ans = a - b;
+                    break;
+                case "*":
+                    ans = a * b;
+                    break;
+                case "/":
+                    if (b == 0)
+                    {
+                        Console.WriteLine("На ноль делить нельзя");
+
+                    }
+                    ans = a / b;
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная операция");
+
+
+                    break;
+
+            }
+            Console.WriteLine($"Ответ: {ans}");
+
+            Console.WriteLine("Нажмите любую клавишу для выполнения программы еще раз");
+            Console.WriteLine("Нажмите Esc для выхода из программы");
+
+            var key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Escape)
+                break;
+
+            Console.ReadKey();
+        }
+    }
+}
